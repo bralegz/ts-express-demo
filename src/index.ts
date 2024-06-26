2a4210484ab57a6776504c5394a7d3e0a1323f53
bralegz <bralegz@gmail.com>
2024-06-26 17:42:10 -0500

    typeorm connected

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import server from "./server";
 import { PORT } from "./config/envs";
+import "reflect-metadata";
+import { AppDataSource } from "./config/data-source";
 
-server.listen(PORT, () => {
-  console.log(`server listening on port ${PORT}`);
-});
+AppDataSource.initialize().then(res => {
+  console.log("Database connected successfully")
+  server.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}`);
+  });
+
+  
+})
+
